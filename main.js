1f395455a0cb84b8d2dad714d1012624857cfc31
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-08-05 18:07:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ leftWristy = 0;
 rightWristx = 0;
 rightWristy = 0;
 
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+statusSong1 = 0;
+statusSong2 = 0;
+
 function setup(){
     canvas = createCanvas(600, 500);
     canvas.center();
@@ -25,6 +31,11 @@ function modelLaoded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Left Wrist Score = " + scoreLeftWrist + "Right Wrist Score = " + scoreRightWrist);
+
         leftWristx = results[0].pose.leftWrist.x;
         leftWristy = results[0].pose.leftWrist.y;
         console.log("Left Wrist x = " + leftWristx + "Left Wrist y = " + leftWristy);
@@ -42,4 +53,22 @@ function preload(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    statusSong1 = music1.isPlaying();
+    statusSong2 = music2.isPlaying();
+
+    fill("#FFFFFF");
+    stroke("#000000");
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristx, leftWristy, 20);
+        music2.stop();
+
+        if(statusSong1 = false){
+            music1.play();
+            document.getElementById("name_of_song").innerHTML = " " + "Harry Potter Theme";
+        }
+    
+    }
+
 }
